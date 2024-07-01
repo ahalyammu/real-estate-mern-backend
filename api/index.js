cd1bca91348638f362e9a1ee34a42cc1ee47ec39
ahalyammu <ahalya.ammu44@gmail.com>
2024-07-01 11:27:04 +0530

    Initial commit

---
+++ api/index.js
@@ -0,0 +1,48 @@
+import express from 'express'
+import mongoose from 'mongoose';
+import cors from 'cors'
+import dotenv from 'dotenv';
+import userRouter from './routes/userRoute.js';
+import authRouter from './routes/authRoute.js';
+import listingRouter from './routes/listingRoute.js';
+import cookieParser from 'cookie-parser';
+import path from 'path'
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
+
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log('Connected to MongoDB');
+}).catch((err)=>{
+    console.log(err);
+})
+
+const __dirname = path.resolve();
+
+app.listen(3000, ()=>{
+    console.log('Server is running on port 3000');
+});
+
+app.use('/api/user',userRouter);
+app.use('/api/auth',authRouter);
+app.use('/api/listing',listingRouter);
+
+app.use(express.static(path.join(__dirname, '/client/dist')))
+
+app.get('*',(req,res,next)=>{
+    res.sendFile(path.join(__dirname, 'client/dist/index.html'))
+})
+
+app.use((err,req,res,next)=>{
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    return res.status(statusCode).json({
+        success:false,
+        statusCode,
+        message
+    })
+});
